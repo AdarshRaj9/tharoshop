07c4587e4711a89e31e98f0b3bb14d9a6d9aecb9
ADARSH <adarshraj998@gmail.com>
2021-03-17 13:24:42 +0530

    10th commit

--- backend/settings.py
+++ backend/settings.py
@@ -12,13 +12,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 #import django_heroku
 # import cloudinary.uploader
-import cloudinary_storage
+# import cloudinary_storage
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 import os
 
@@ -32,9 +33,9 @@ SECRET_KEY = 'om8%6d5w&^^418ls@1&%8h3m+t7knm5y(4)cqs=edfxrk@yu&v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =['tharoshop.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS =['tharoshop.herokuapp.com','127.0.0.1','localhost']
 
-# DEBUG = True
+DEBUG = True
 
 # ALLOWED_HOSTS =[]
 
@@ -51,8 +52,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'storages',
+
     'base.apps.BaseConfig',
-    'cloudinary_storage',
+    #'cloudinary_storage',
 
 
 
@@ -99,6 +102,9 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+ 
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -107,7 +113,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     #'django.middleware.common.CommonMiddleware',
 ]
@@ -182,25 +187,29 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-# STATICFILES_DIRS =[
-#     BASE_DIR / 'static',
-#     BASE_DIR / 'frontend/build/static'
-# ]
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend/build/static'),
-)
+STATICFILES_DIRS =[
+    BASE_DIR / 'static',
+    BASE_DIR / 'frontend/build/static'
+]
 
-MEDIA_ROOT = 'static/images '
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'frontend/build/static'),
+# )
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 CORS_ALLOW_ALL_ORIGINS=True
@@ -208,8 +217,11 @@ CORS_ALLOW_ALL_ORIGINS=True
 
 #django_heroku.settings(locals())
 
-cloudinary.config( 
-  cloud_name = "dgtpqurhd", 
-  api_key = "558793867666724", 
-  api_secret = "EV-P88doYqaEiUFd_B8Gr6wCBV0" 
-)
+# cloudinary.config( 
+#   cloud_name = "dgtpqurhd", 
+#   api_key = "558793867666724", 
+#   api_secret = "EV-P88doYqaEiUFd_B8Gr6wCBV0" 
+# )
+
+if os.getcwd() == '/app':
+    DEBUG = False
--- base/models.py
+++ base/models.py
@@ -10,8 +10,8 @@ class Product(models.Model):
     user=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
 
     name=models.CharField(max_length=200,null=True,blank=True)
-    #image=models.ImageField(null=True,blank=True,default='/placeholder.png')
-    image=CloudinaryField('image',null=True,blank=True,default='/placeholder.png')
+    image=models.ImageField(null=True,blank=True,default='/placeholder.png')
+    #image=CloudinaryField('image',null=True,blank=True,default='/placeholder.png')
     brand=models.CharField(max_length=200,null=True,blank=True)
     category=models.CharField(max_length=200,null=True,blank=True)
     description=models.TextField(null=True,blank=True)
@@ -67,8 +67,8 @@ class OrderItem(models.Model):
     name=models.CharField(max_length=200,null=True,blank=True)
     qty=models.IntegerField(null=True,blank=True,default=0)
     price=models.DecimalField(max_digits=7,decimal_places=2,null=True,blank=True)
-    #image=models.CharField(max_length=200,null=True,blank=True)
-    image=CloudinaryField('image',max_length=200,null=True,blank=True)
+    image=models.CharField(max_length=200,null=True,blank=True)
+    #image=CloudinaryField('image',max_length=200,null=True,blank=True)
 
     _id=models.AutoField(primary_key=True,editable=False)
 
